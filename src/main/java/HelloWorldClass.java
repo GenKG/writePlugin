df87dc3a55deacd5996061897c9284b9bb0020f4
Deni Baskovsky <qertis@users.noreply.github.com>
2021-11-22 12:46:19 +0300

    пример селекшина

--- src/main/java/HelloWorldClass.java
+++ src/main/java/HelloWorldClass.java
@@ -1,12 +1,24 @@
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.popup.JBPopupFactory;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Locale;
 
 public class HelloWorldClass extends AnAction {
     @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
-        JBPopupFactory factory = JBPopupFactory.getInstance();
-        factory.createMessage("Hello, World!").showInFocusCenter();
+
+    }
+    @Override
+    public void update(@NotNull final AnActionEvent e) {
+      // Get required data keys
+      final Project project = e.getProject();
+      final Editor editor = e.getData(CommonDataKeys.EDITOR);
+      JBPopupFactory factory = JBPopupFactory.getInstance();
+      factory.createMessage(editor.getCaretModel().getCurrentCaret().getSelectedText().toUpperCase(Locale.ROOT)).showInFocusCenter();
     }
 }
