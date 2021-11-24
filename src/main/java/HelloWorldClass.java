5d6455e65bdf12204857eff6950f6591514bf9db
Deni Baskovsky <qertis@users.noreply.github.com>
2021-11-24 20:37:05 +0300

    Отображение текста в Event Log

--- src/main/java/HelloWorldClass.java
+++ src/main/java/HelloWorldClass.java
@@ -1,3 +1,5 @@
+import com.intellij.notification.NotificationGroupManager;
+import com.intellij.notification.NotificationType;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
@@ -5,10 +7,13 @@ import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.popup.JBPopupFactory;
 import org.jetbrains.annotations.NotNull;
+import com.intellij.openapi.diagnostic.Logger;
 
 import java.util.Locale;
 
 public class HelloWorldClass extends AnAction {
+//     private static final Logger LOG = Logger.getInstance(HelloWorldClass.class);
+
     @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
 
@@ -20,5 +25,12 @@ public class HelloWorldClass extends AnAction {
       final Editor editor = e.getData(CommonDataKeys.EDITOR);
       JBPopupFactory factory = JBPopupFactory.getInstance();
       factory.createMessage(editor.getCaretModel().getCurrentCaret().getSelectedText().toUpperCase(Locale.ROOT)).showInFocusCenter();
+
+       NotificationGroupManager.getInstance().getNotificationGroup("Custom Notification Group")
+            .createNotification("My content", NotificationType.INFORMATION)
+            .notify(project);
+
+//      LOG.info("test message");
+//      LOG.error("ZXXXX");
     }
 }
