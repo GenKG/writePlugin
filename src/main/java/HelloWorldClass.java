8376a8d775a21e859066004f539149b1ce94b056
Deni Baskovsky <qertis@users.noreply.github.com>
2021-12-02 12:57:45 +0300

    Отправка сообщения на сервис proofread

--- src/main/java/HelloWorldClass.java
+++ src/main/java/HelloWorldClass.java
@@ -7,30 +7,63 @@ import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.popup.JBPopupFactory;
 import org.jetbrains.annotations.NotNull;
-import com.intellij.openapi.diagnostic.Logger;
-
-import java.util.Locale;
+import java.io.*;
+import java.net.*;
 
 public class HelloWorldClass extends AnAction {
-//     private static final Logger LOG = Logger.getInstance(HelloWorldClass.class);
+
+    private String glvrdProofRead(String text, String key) throws Exception {
+        URL url = new URL("https://glvrd.ru/api/v3/proofread/");
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestProperty("X-GLVRD-KEY", key);
+        con.setUseCaches(true);
+        con.setDoOutput(true);
+        con.setDoInput(true);
+        con.setRequestMethod("POST");
+
+        String urlParameters = "text=" + text;
+        OutputStreamWriter writer = new OutputStreamWriter(con.getOutputStream());
+        writer.write(urlParameters);
+        writer.close();
+
+        int status = con.getResponseCode();
+        switch (status) {
+            case 200:
+            case 201:
+                BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                StringBuilder sb = new StringBuilder();
+                String line;
+                while ((line = br.readLine()) != null) {
+                    sb.append(line + "\n");
+                }
+                br.close();
+
+                return sb.toString();
+        }
+        return "";
+    }
 
     @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
 
     }
+
     @Override
     public void update(@NotNull final AnActionEvent e) {
-      // Get required data keys
-      final Project project = e.getProject();
-      final Editor editor = e.getData(CommonDataKeys.EDITOR);
-      JBPopupFactory factory = JBPopupFactory.getInstance();
-      factory.createMessage(editor.getCaretModel().getCurrentCaret().getSelectedText().toUpperCase(Locale.ROOT)).showInFocusCenter();
+        // Get required data keys
+        final Project project = e.getProject();
+        final Editor editor = e.getData(CommonDataKeys.EDITOR);
+        JBPopupFactory factory = JBPopupFactory.getInstance();
+        String selectionText = editor.getCaretModel().getCurrentCaret().getSelectedText();
+//        factory.createMessage(editor.getCaretModel().getCurrentCaret().getSelectedText().toUpperCase(Locale.ROOT)).showInFocusCenter();
 
-       NotificationGroupManager.getInstance().getNotificationGroup("Custom Notification Group")
-            .createNotification("My content", NotificationType.INFORMATION)
-            .notify(project);
-
-//      LOG.info("test message");
-//      LOG.error("ZXXXX");
+        try {
+            String result = this.glvrdProofRead(selectionText, "KEY");
+            NotificationGroupManager.getInstance().getNotificationGroup("Custom Notification Group")
+                    .createNotification(result, NotificationType.INFORMATION)
+                    .notify(project);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 }
