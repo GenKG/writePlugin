92c60308c0c8532f7e8a0daaf450267a5bdf3436
Deni Baskovsky <qertis@users.noreply.github.com>
2021-12-02 20:53:11 +0300

    Обработка комментариев и уведомление под комментарием

---
+++ src/main/java/MySpellChecking.java
@@ -0,0 +1,62 @@
+import com.intellij.codeInspection.*;
+import com.intellij.lang.*;
+import com.intellij.lang.injection.InjectedLanguageManager;
+import com.intellij.openapi.util.TextRange;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiElementVisitor;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.tree.IElementType;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public final class MySpellChecking extends LocalInspectionTool {
+    private static GLVRD glvrd = new GLVRD();
+
+    @Override
+    public SuppressQuickFix @NotNull [] getBatchSuppressActions(@Nullable PsiElement element) {
+        return super.getBatchSuppressActions(element);
+    }
+
+    @Override
+    public boolean isSuppressedFor(@NotNull PsiElement element) {
+        return super.isSuppressedFor(element);
+    }
+
+    @Override
+    @NotNull
+    public PsiElementVisitor buildVisitor(@NotNull final ProblemsHolder holder, final boolean isOnTheFly) {
+        return new PsiElementVisitor() {
+            @Override
+            public void visitElement(@NotNull final PsiElement element) {
+                if (holder.getResultCount() > 1000) return;
+
+                final ASTNode node = element.getNode();
+                if (node == null) {
+                    return;
+                }
+
+                // Extract parser definition from element
+                final IElementType elementType = node.getElementType();
+
+                PsiFile containingFile = element.getContainingFile();
+                if (containingFile != null && Boolean.TRUE.equals(containingFile.getUserData(InjectedLanguageManager.FRANKENSTEIN_INJECTION))) {
+                    return;
+                }
+
+                String elementText = element.getText();
+
+                if (elementText.length() > 5 && elementText.startsWith("//") && elementType.toString().equals("END_OF_LINE_COMMENT")) {
+                    try {
+                        String result = glvrd.glvrdProofRead(elementText, "KEY");
+                        TextRange textRange = new TextRange(0, 5); // todo исправить text range от ответа главреда
+
+                        ProblemDescriptorBase problemDescriptor = new ProblemDescriptorBase(element, element, "Коммент не очень. Исправьте: " + result, null, ProblemHighlightType.GENERIC_ERROR_OR_WARNING, false, textRange, true, isOnTheFly);
+                        holder.registerProblem(problemDescriptor);
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                    }
+                }
+            }
+        };
+    }
+}
