bc0459e266e5c2589b50503ad596b27afb9ae5ac
Deni Baskovsky <qertis@users.noreply.github.com>
2022-01-04 12:13:43 +0300

    Добавил ссылку на документацию

--- src/main/java/AppSettingsComponent.java
+++ src/main/java/AppSettingsComponent.java
@@ -1,3 +1,4 @@
+import com.intellij.ui.components.BrowserLink;
 import com.intellij.ui.components.JBLabel;
 import com.intellij.ui.components.JBTextField;
 import com.intellij.util.ui.FormBuilder;
@@ -9,13 +10,14 @@ import javax.swing.*;
  * Supports creating and managing a {@link JPanel} for the Settings Dialog.
  */
 public class AppSettingsComponent {
-
     private final JPanel myMainPanel;
     private final JBTextField glvrdKeyText = new JBTextField();
+    private final JButton glvrdAPILink = new BrowserLink("glvrd.ru", "https://glvrd.ru/api/");
 
     public AppSettingsComponent() {
         myMainPanel = FormBuilder.createFormBuilder()
                 .addLabeledComponent(new JBLabel("Ключ для запросов: "), glvrdKeyText, 1, false)
+                .addLabeledComponent(new JBLabel("Powered by"), glvrdAPILink, 2, false)
                 .addComponentFillVertically(new JPanel(), 0)
                 .getPanel();
     }
@@ -36,5 +38,4 @@ public class AppSettingsComponent {
     public void setUserNameText(@NotNull String newText) {
         glvrdKeyText.setText(newText);
     }
-
 }
